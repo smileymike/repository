e2acdf692d75e3e39297ac88121cb410da884894
Michael Cunnane <michael.cunnane@gmail.com>
2020-09-30 00:43:46 +0100

    Add content abt setting up git and ssh on linux. Also adding ssh to github

--- Chapter 2 - Using Atom/github-package.py
+++ Chapter 2 - Using Atom/github-package.py
@@ -12,4 +12,27 @@ $ git config --global core.editor "atom --wait"
 To check details:
 $ git config --list
 
-1. "Generating Your SSH Public Key" at Github.com
+1. Checking for existing SSH Keys
+$ ls -al ~/.ssh
+
+2. "Generating Your SSH Public Key"
+$ ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+id_rsa and rd_rsa.ub files should be created in .ssh folder
+
+3. Adding your SSH key to the ssh-agent
+$ eval "$(ssh-agent -s)"
+> Agent pid 59566
+
+$ ssh-add ~/.ssh/id_rsa
+
+4. Adding a new SSH key to your github account via "setting" -> "SSH and GPG Keys"
+
+5. Testing your SSH connection
+$ ssh -T git@github.
+If problems see a link below:
+https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/testing-your-ssh-connection
+
+6.
+
+'''
