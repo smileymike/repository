72292eaa95903e07ad3fcd91d4796b8711f8d9e8
Michael Cunnane <michael.cunnane@gmail.com>
2020-09-29 23:55:08 +0100

    Added two files

---
+++ Chapter 2 - Using Atom/github-package.py
@@ -0,0 +1,15 @@
+# github-package.py
+
+'''Set up Github on Linux
+
+If have not set up git on linux.
+$ git config --global user.name "<Your Name>"
+$ git config --global user.email <your email address>
+
+if you are using atom editor:
+$ git config --global core.editor "atom --wait"
+
+To check details:
+$ git config --list
+
+1. "Generating Your SSH Public Key" at Github.com
---
+++ Chapter 2 - Using Atom/version-control-in-atom.py
@@ -0,0 +1,13 @@
+# version-control-in-atom.py
+'''Version control is an important aspect of any project and Atom comes with
+basic Git and GitHub integration built in.
+
+In order to use version control in Atom, the project root needs to contain the
+Git repository.
+'''
+
+# Checkout HEAD status
+# Alt+Ctrl+Z
+
+# Git status list
+# Ctrl+Shift+B 
