dfd3e29d5288b9a6d23d4ac223841b550ef30a02
Michael Cunnane <michael.cunnane@gmail.com>
2020-09-30 14:18:37 +0100

    file renamed, created new file

---
+++ Chapter 2 - Using Atom/github-setup.py
@@ -0,0 +1,43 @@
+# github-package.py
+
+'''Set up Github on Linux
+
+If have not set up git on linux.
+$ git config --global user.name "<Your Name>"
+$ git config --global user.email <your email address>
+
+if you are using atom editor:
+$ git config --global core.editor "atom --wait"
+
+To check details:
+$ git config --list
+
+1. Checking for existing SSH Keys
+$ ls -al ~/.ssh
+
+2. "Generating Your SSH Public Key"
+$ ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+id_rsa and rd_rsa.ub files should be created in .ssh folder
+
+3. Adding your SSH key to the ssh-agent
+$ eval "$(ssh-agent -s)"
+> Agent pid 59566
+$ ssh-add ~/.ssh/id_rsa
+
+4. Adding a new SSH key to your github account via "setting" -> "SSH and GPG Keys"
+
+5. Testing your SSH connection
+$ ssh -T git@github.
+If problems see a link below:
+https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/testing-your-ssh-connection
+
+6. Create a repository at your github account website to get a link
+
+7. Add Origin in source code folder with .git folder
+$ git remote add origin https://github.com/<github account>/<your repository name>.git
+
+8. To check
+$ git remote -v
+
+'''
