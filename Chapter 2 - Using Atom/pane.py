0fb5a14c2b9d41fadfff88216767c4b02fde2280
Michael Cunnane <michael.cunnane@gmail.com>
2020-09-30 14:56:05 +0100

    Add further content

---
+++ Chapter 2 - Using Atom/pane.py
@@ -0,0 +1,5 @@
+# pane.py
+'''To close a pane, you can close all pane items with Ctrl+W. You can configure
+whether panes auto-close when empty in the Settings View with the "Remove Empty
+Panes" setting under Core Settings.
+'''
--- Chapter 2 - Using Atom/version-control-in-atom.py
+++ Chapter 2 - Using Atom/version-control-in-atom.py
@@ -7,7 +7,40 @@ Git repository.
 '''
 
 # Checkout HEAD status
-# Alt+Ctrl+Z        yet to work
+# Alt+Ctrl+Z        yet to work = "git checkout HEAD --<path>
+# and git reset HEAD -- <path>"
 
 # Git status list
-# Ctrl+Shift+B
+# Ctrl+Shift+B      is equal to "git status" which displays a list of all the
+# untracked and modified files in the project
+
+# Quickly open files in the project
+# Ctrl + B
+
+# Status bar icons for short cut
+# "master"
+# "push"
+# "github"
+# "git"
+
+# Line diffs
+# The included git-diff package colorizes the gutter next to lines that have
+# been added or edited (green highlight), or removed(red highlight) when
+# selected any either unstaged or staged "changed" files in "git" pane.
+
+'''Open on GitHub
+
+If the project you're working on is on GitHub, there are also some very useful
+integrations you can use. Most of the commands will take the current file you're
+viewing and open a view of that file on GitHub - for instance, the blame or
+commit history of that file.
+
+    Alt+G O - Open file on GitHub
+    Alt+G B - Open Blame view of file on GitHub
+    Alt+G H - Open History view of file on GitHub
+    Alt+G C - Copy the URL of the current file on GitHub to the clipboard
+    Alt+G R - Branch compare on GitHub
+
+The branch comparison shows you the commits that are on the branch you're
+currently working on locally that are not on the mainline branch.
+'''
