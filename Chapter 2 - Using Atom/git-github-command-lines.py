8da5f326bb76110f442843513d3c92414b3c4208
Michael Cunnane <michael.cunnane@gmail.com>
2020-09-30 15:13:47 +0100

    Adding contents from atom website on github packages

---
+++ Chapter 2 - Using Atom/git-github-command-lines.py
@@ -0,0 +1,3 @@
+# git-github-command-lines.py
+
+https://www.youtube.com/watch?v=SWYqp7iY_Tc
--- Chapter 2 - Using Atom/github-package.py
+++ Chapter 2 - Using Atom/github-package.py
@@ -1,1 +1,41 @@
 # github-package.py
+'''The github package brings Git and GitHub integration right inside Atom.
+
+    Initialize
+    Clone
+    Branch
+    Stage
+    Discard
+    Commit
+    Amend and undo
+    Publish and push
+    Fetch and pull
+    Resolve conflicts
+    Create a Pull Request
+    View Pull Requests
+    Checkout a Pull Request
+    Open any Issue or Pull Request
+    View Pull Request review comments
+    Navigate Pull Request review comments
+    Respond to a Pull Request review comment
+'''
+# Open the Git Panel:
+# Ctrl+9
+
+# Open the GitHub Panel:
+# Ctrl+8
+
+# Initalise
+# In case a project doesn't have a Git repository yet, you can create one from
+# the Git panel.
+
+# Clone repositories
+# To clone a repository, open the GitHub panel while you have no project folders
+# open in Atom and click "Clone an existing GitHub repository". In the dialog,
+# paste the URL of a repository and click "Clone". The new project will be added
+# to the Tree View.
+# Alternately, run the GitHub: Clone command to open the Clone dialog any time.
+
+# Branch
+# To open the branch tooltip, click the branch icon in the Status Bar. From
+# there you can create or switch branches.
